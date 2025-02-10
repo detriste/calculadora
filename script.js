a28783e0d719f0fabe88c2221aaeecff3a50e242
detriste <detriste2018@gmail.com>
2025-02-10 16:52:13 -0300

    primeiro commit

--- script.js
+++ script.js
@@ -1,52 +1,79 @@
-//const prompt = require('prompt-sync');
-const PromptSync = require('prompt-sync');
-const prompt = PromptSync();
+// const prompt = require('prompt-sync')();
 
-function subtracao1(a, b) {
+const promptSync = require('prompt-sync');
+const prompt = promptSync();
+
+function soma(a, b) {
+    return a + b;
+}
+
+function subtracao(a, b) {
     return a - b;
 }
 
-function soma2(a, b) {
-    return a + b;
-}
-function mult3(a, b) {
+function multiplicacao(a, b) {
     return a * b;
 }
-function div4(a, b) {
+
+function divisao(a, b) {
     if (b !== 0) {
         return a / b;
     }
     else {
-        return "erro dividido por zero";
+        return "Erro: Divisão por zero";
     }
+}
 
+function raizquadrada(a){
+    return Math.sqrt(a)
+}
+
+function exponenciacao(a, b){
+    return Math.pow(a, b)
+}
+
+function seno(a){
+    return Math.sin(a)
+}
+
+function cosseno(a){
+    return Math.cos(a)
+}
+function tangente(a){
+    return Math.tan(a)
 }
 
 
 while (true) {
-    let escolha = prompt("escolha a operacâo 1)soma,2)sdivição,3)multiplicação,4)divisão")
-
-
-    if (escolha === "5") {
+    let escolha = prompt("Escolha a operação: 1. Soma, 2. Subtração, 3. Multiplicação, 4. divisão, 5. Raiz quadrada, 6. Exponenciação, 7. Seno, 8. Cosseno, 9. Tangente, 10. Sair");
+    console.log("Em caso de Raiz quadrade, Seno, Cosseno ou Tangente, selecione apenas um número. ");
+    if (escolha === "10") {
         break;
     }
 
-
-
-
-    let nun1 = parseFloat(prompt("digite o primeiro numero"))
-    let nun2 = parseFloat(prompt("digite o segundo numero"))
+    let num1 = parseFloat(prompt("Digite o primeiro número: "));
+    let num2 = parseFloat(prompt("Digite o segundo número: "));
 
     if (escolha === "1") {
-        console.log("resultado", soma2(nun1, nun2));
+        console.log("Resultado: ", soma(num1, num2));
+    } else if (escolha === "2") {
+        console.log("Resultado: ", subtracao(num1, num2));
+    } else if (escolha === "3") {
+        console.log("Resultado: ", multiplicacao(num1, num2));
+    } else if (escolha === "4") {
+        console.log("Resultado: ", divisao(num1, num2));
+    } else if (escolha === "5") {
+        console.log("Resultado: ", raizquadrada(num1))
+    } else if (escolha === "6") {
+        console.log("Resultado: ", exponenciacao(num1, num2))
+    } else if (escolha === "7") {
+        console.log("Resultado: ", seno(num1))
+    } else if (escolha === "8") {
+        console.log("Resultado: ", cosseno(num1))
+    } else if (escolha === "9") {
+        console.log("Resultado: ", tangente(num1))
     }
-    else if (escolha === "2") {
-        console.log("resultado", subtracao1(nun1, nun2));
-    }
-    else if (escolha === "3") {
-        console.log("resultado", mult3(nun1, nun2));
-    }
-    else if (escolha === "4") {
-        console.log("resultado", div4(nun1, nun2));
-    }
+    else {
+        console.log("Escolha inválida");
+    } 
 }
